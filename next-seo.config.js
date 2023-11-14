9f5ecc7c7ae476bdd9cc3a0937ea983fd28800cd
saeed madi <150622499+madman9494@users.noreply.github.com>
2023-11-14 19:06:18 +0330

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -11,3 +11,16 @@ export default {
     cardType: 'summary_large_image',
   }
 }
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: process.env.NEXT_PUBLIC_SITE_URL || 'https://lilnouns.dev',
+    site_name: 'Lil Nouns Pics',
+  },
+  twitter: {
+    handle: '@nekofar',
+    site: '@nekofar',
+    cardType: 'summary_large_image',
+  }
+}
